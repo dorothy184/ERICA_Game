680a1fa378a85a8e4e981481fec576f564e3fc8f
mara <dorothy29148@gmail.com>
2019-11-24 16:25:39 +0900

    map

---
+++ unity/Assets/mi.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+    public void Start()
+    {
+        Invoke("MakeGuiding", .1f);
+    }
+
+    // Update is called once per frame
+    void MakeGuiding()
+    {
+        Application.LoadLevel("guide");
+    }
+}
